df2a7c76cb39fe297d517ed437c5a716808f3d8b
Arlie Rivas <arliedamir@gmail.com>
2024-08-16 00:06:08 -0500

    Refactor color scheme selector

--- color_scheme_selector.py
+++ color_scheme_selector.py
@@ -1,76 +1,107 @@
 import os
 import json
 import toml
-import time
 
 # Load the JSON data from the file
-home = os.environ['HOME']
-with open(f'{home}/.config/alacritty/color_schemes.json', 'r') as file:
-    data = json.load(file)
-
-# Get the keys from the 'index' dictionary and add them to an array
-schemes = list(data['schemes'].keys())
+def load_schemes():
+    home = os.environ['HOME']
+    with open(f'{home}/.config/color_schemes.json', 'r') as file:
+        data = json.load(file)
+    return data
 
 # Show the available schemes
-print("\033[94m" + "Available schemes:" + "\033[0m")
-for i, scheme in enumerate(schemes):
-    print(f"[{i}] {scheme}")
+def show_schemes(schemes):
+    print("\033[94m" + "Available schemes:" + "\033[0m")
+    for i, scheme in enumerate(schemes):
+        print(f"[{i}] {scheme}")
 
-print("\033[31m" + "Warning: " + "\033[0m" + "This script will close Zellij if it is running.")
 # Validate the input
-while True:
-    current_scheme = input("\033[94m" + "Please enter the color scheme number or 'x' to exit: " + "\033[0m")
-    if current_scheme.lower() == 'x':
-        print("Exiting...")
-        exit(0)
-    try:
-        current_scheme = int(current_scheme)
-    except ValueError:
-        print("\033[31m" + "Invalid input. Please enter an integer." + "\033[0m")
-        continue
-    if current_scheme >= len(schemes):
-        print("\033[31m" + "Invalid input. The number must be less than " + str(len(schemes)) + "\033[0m")
-        continue
-    break
+def select_scheme(schemes):
+    while True:
+        current_scheme = input("\033[94m" + "Please enter the color scheme number or 'x' to exit: " + "\033[0m")
+        if current_scheme.lower() == 'x':
+            print("Exiting...")
+            exit(0)
+        try:
+            current_scheme = int(current_scheme)
+        except ValueError:
+            print("\033[31m" + "Invalid input. Please enter an integer." + "\033[0m")
+            continue
+        if current_scheme >= len(schemes):
+            print("\033[31m" + "Invalid input. The number must be less than " + str(len(schemes)) + "\033[0m")
+            continue
+        break
+    return current_scheme
 
-scheme_data = data['schemes'][schemes[current_scheme]]
-toml_string = toml.dumps(scheme_data)
+# Save the TOML file
+def save_toml_file(scheme_data):
+    home = os.environ['HOME']
+    toml_string = toml.dumps(scheme_data)
+    with open(f'{home}/.config/alacritty/current_scheme.toml', 'w') as file:
+        file.write(toml_string)
 
-with open(f'{home}/.config/alacritty/current_scheme.toml', 'w') as file:
-    file.write(toml_string)
+def is_zellij_installed():
+    return os.system('which zellij > /dev/null 2>&1') == 0
 
-# Verificar si Zellij está instalado
-zellij_installed = os.system('which zellij > /dev/null 2>&1')
-
-if zellij_installed == 0:
+def map_color_scheme(scheme_data):
     primary_colors = scheme_data['colors']['primary']
     normal_colors = scheme_data['colors']['normal']
+
+    color_mappings = {
+        'fg': 'foreground',
+        'bg': 'background',
+        'orange': 'red',
+    }
 
     color_properties = [
         'fg', 'bg', 'red', 'green', 'yellow', 'blue', 'cyan', 'magenta', 'orange', 'black', 'white'
     ]
 
-    color_values = {
-        'fg': primary_colors['foreground'],
-        'bg': primary_colors['background'],
-        'red': normal_colors['red'],
-        'green': normal_colors['green'],
-        'yellow': normal_colors['yellow'],
-        'blue': normal_colors['blue'],
-        'cyan': normal_colors['cyan'],
-        'magenta': normal_colors['magenta'],
-        'orange': normal_colors['red'],
-        'black': normal_colors['black'],
-        'white': normal_colors['white'],
-    }
-
     color_string = "themes {\n   default {\n"
     for prop in color_properties:
-        color_string += f"      {prop} \"{color_values[prop]}\"\n"
+        if prop in ['fg', 'bg',]:
+            color_string += f"      {prop} \"{primary_colors[color_mappings[prop]]}\"\n"
+        elif prop in ['orange']:
+            color_string += f"      {prop} \"{normal_colors[color_mappings[prop]]}\"\n"
+        else:
+            color_string += f"      {prop} \"{normal_colors[prop]}\"\n"
     color_string += "   }\n}"
 
-    # Guardar el archivo KDL
+    return color_string
+
+def save_kdl_file(color_string):
+    home = os.environ['HOME']
     with open(f'{home}/.config/zellij/theme/scheme.kdl', 'w') as file:
-        file.write(color_string)
-    
+        file.write(color_string)   
+
+def set_zellij_color_scheme(scheme_data):
+    color_string = map_color_scheme(scheme_data)
+    save_kdl_file(color_string)
     os.system('zellij ka -y > /dev/null 2>&1')
+
+def get_user_confirmation():
+    while True:
+        user_input = input("\033[31m" + "Warning: " + "\033[0m" + "The script will close Zellij if it is running. Do you want to continue? (y/n): ").strip().lower()
+        if user_input in ['y', 'n']:
+            return user_input == 'y'
+        print("Invalid input. Please enter 'y' or 'n'.")
+
+def set_color_schemes():
+    print("\033[32m" + "Info: " + "\033[0m" + "This script will change the color schemes for Zellij, WezTerm, and Alacritty.")
+
+    if not get_user_confirmation():
+        print("Operation cancelled by the user.")
+        return
+
+    data = load_schemes()
+    schemes = list(data['schemes'].keys())
+    show_schemes(schemes)
+    current_scheme = select_scheme(schemes)
+    scheme_data = data['schemes'][schemes[current_scheme]]
+    print(scheme_data)
+    save_toml_file(scheme_data)
+
+    if is_zellij_installed():
+        set_zellij_color_scheme(scheme_data)
+
+set_color_schemes()
