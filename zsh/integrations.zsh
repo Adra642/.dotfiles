181f06c5fc7aada1a1fcdcea4d0878c1b573d035
Arlie Rivas <arliedamir@gmail.com>
2026-05-21 14:42:50 +0200

    Add tmux autostart integration

--- zsh/integrations.zsh
+++ zsh/integrations.zsh
@@ -5,12 +5,13 @@
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 # Zellij - Terminal multiplexer
-if [[ -z "$ZELLIJ" && -x "$(command -v zellij)" ]]; then
-    exec zellij attach -c default
-fi
+# if [[ -z "$ZELLIJ" && -x "$(command -v zellij)" ]]; then
+#    exec zellij attach -c default
+# fi
 
 # Fzf - Fuzzy finder
 [[ -x "$(command -v fzf)" ]] && source <(fzf --zsh)
 
-# Bun - JavaScript runtime (completions)
-[[ -s "$BUN_INSTALL/_bun" ]] && source "$BUN_INSTALL/_bun"
+if [[ -z "$TMUX" ]] && [[ $- == *i* ]] && command -v tmux &> /dev/null; then
+    tmux attach-session -t main || tmux new-session -s main
+fi
